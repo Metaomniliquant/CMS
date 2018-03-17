32e39c445a6441bcad3a52dce34f99d67ff7bdda
metaomniliquant <asusultra@live.com>
2018-03-17 11:43:15 -0500

    remove server js files and add to gitignore

--- server/data/schema/Todo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var mongoose = require("mongoose");
-var todoSchema = new mongoose.Schema({
-    text: String,
-    description: String,
-    done: Boolean
-});
-exports.Todo = mongoose.model('Todo', todoSchema);
